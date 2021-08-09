a5b631d0131f64708c337c4472c0b0cfaca79f5b
dbsqwerty123 <sean.seah0311@gmail.com>
2021-08-09 22:41:41 +0800

    email list

--- htdocs/admin/actions/new_email_whitelist.php
+++ htdocs/admin/actions/new_email_whitelist.php
@@ -9,17 +9,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     validate_xsrf_token($_POST[CONST_XSRF_TOKEN_KEY]);
 
     if ($_POST['action'] == 'new') {
-
       //convert emails to array (comma delimited). Iterate thru each email/add it
+      $email_raw = $_POST['email'];
+      $emails = explode(',',$email_raw);
+      $whitelist_post = $_POST['whitelist'];
+      $enabled_post = $_POST['enabled'];
 
+      foreach($emails as $email_arr){
        $id = db_insert(
           'email_list',
           array(
-             'email'=>$_POST['email'],
-             'white'=>$_POST['whitelist'],
-             'enabled'=>$_POST['enabled']
+             'email'=>$email_arr,
+             'white'=>$whitelist_post,
+             'enabled'=>$enabled_post
           )
        );
+      }
 
        if ($id) {
           redirect('/admin/list_email_whitelist.php?generic_success=1');
