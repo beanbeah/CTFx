2edc1963798be6c9b53bac0d2bd6cb6d28a86ce3
root <sean.seah0311@gmail.com>
2021-08-09 07:46:50 +0000

    colors and bootstrap

--- include/layout/layout.inc.php
+++ include/layout/layout.inc.php
@@ -30,7 +30,7 @@ function head($title = '') {
     <link rel="icon" href="/img/favicon.png" type="image/png" />
 
     <!-- CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/mellivora.css?ver=' . $staticVersion . '" rel="stylesheet">';
 
     js_global_dict();
