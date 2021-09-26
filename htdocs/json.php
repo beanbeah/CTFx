a448532848349893fffe54098a06c1ce83c1461f
dbsqwerty <sean.seah0311@gmail.com>
2021-09-26 18:32:47 +0800

    scoreboard history API done??

--- htdocs/json.php
+++ htdocs/json.php
@@ -37,6 +37,14 @@ if ($_GET['view'] == 'scores') {
         cache_end(CONST_CACHE_NAME_SCORES_JSON);
     }
 }
+else if ($_GET['view'] == 'graph') {
+    if (cache_start(CONST_CACHE_NAME_SCORES_JSON, Config::get('MELLIVORA_CONFIG_CACHE_TIME_SCORES'))) {
+        json_score_dump();
+        // To make the scoreboard fully CTFTime compatible you can run this python3 one-liner to encode unicode chars to \uXXXX:
+        // open("ctftime.json","wb").write(open("ctfx.json","r",encoding='utf-8').read().encode("ascii","backslashreplace").replace(b"\\x",b"\\u00"))
+        cache_end(CONST_CACHE_NAME_SCORES_JSON);
+    }
+}
 
 else {
     echo json_error(lang_get('please_request_view'));
--- include/json.inc.php
+++ include/json.inc.php
@@ -42,3 +42,49 @@ function json_scoreboard ($user_type = null) {
 
     echo json_encode($scoreboard);
 }
+
+function json_score_dump () {
+    $export = array();
+
+    //first retrieve top 10 so we dont actually die
+    $consolidated_scores = db_query_fetch_all('
+        SELECT
+           u.id AS user_id,
+           u.team_name,
+           SUM(c.points) AS score,
+           MAX(s.added) AS tiebreaker
+        FROM users AS u
+        LEFT JOIN submissions AS s ON u.id = s.user_id AND s.correct = 1
+        LEFT JOIN challenges AS c ON c.id = s.challenge
+        WHERE
+          u.competing = 1
+          '.(is_valid_id($user_type) ? 'AND u.user_type = :user_type' : '').'
+        GROUP BY u.id
+        ORDER BY score DESC, tiebreaker ASC');
+
+    for ($i =0; $i < 10; $i++){
+        $challenges_solved = db_query_fetch_all('
+            SELECT 
+                submissions.added AS time_solve, 
+                challenges.points
+            FROM submissions INNER JOIN challenges 
+            ON
+                submissions.user_id = ' . $consolidated_scores[$i]['user_id'] .' AND
+                submissions.correct = 1 AND
+                submissions.challenge = challenges.id
+            ORDER BY time_solve ASC');
+
+        $export[$i]['position'] = $i + 1;
+        $export[$i]['team_name'] = $consolidated_scores[$i]['team_name'];
+
+        //consolidate scores
+        $sum = 0;
+        for ($j = 0; $j<count($challenges_solved); $j++){
+            $sum += $challenges_solved[$j]['points'];
+            $time = $challenges_solved[$j]['time_solve'];
+            $export[$i]['score_time'][$j] = array($time,$sum);
+        }
+    }
+
+    echo json_encode($export);
+}
