3f6c1a930141811ed2dfebb33b99c03b6ad78941
dbsqwerty <sean.seah0311@gmail.com>
2021-10-09 15:06:15 +0800

    added jquery back

--- include/layout/layout.inc.php
+++ include/layout/layout.inc.php
@@ -141,6 +141,7 @@ function foot()
 </div> <!-- /page -->
 
 <!-- JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 <script type="text/javascript" src="/js/mellivora.js?ver=' . $staticVersion . '"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
